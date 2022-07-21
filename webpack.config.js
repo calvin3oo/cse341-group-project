817a47fcffe54388d3b34b75adc6ddd8c0b229a9
calvin3oo <86371905+calvin3oo@users.noreply.github.com>
2022-07-21 00:00:19 -0600

    a

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,23 @@ const path = require('path');
 
 module.exports = [
     {
-      entry: './Static/js/index.js',              //entry
+      entry: './static/js/index.js',              //entry
       output: {
-        path: path.resolve(__dirname, 'Static/dist'),    //output where to put/what to name webpack folder
+        path: path.resolve(__dirname, 'static/dist'),    //output where to put/what to name webpack folder
         filename: 'index.js'
       },
     },
     {
-      entry: './Static/js/login.js',              //entry
+      entry: './static/js/login.js',              //entry
       output: {
-        path: path.resolve(__dirname, 'Static/dist'),    //output where to put/what to name webpack folder
+        path: path.resolve(__dirname, 'static/dist'),    //output where to put/what to name webpack folder
         filename: 'login.js'
       },
     },
     {
-      entry: './Static/js/signup.js',              //entry
+      entry: './static/js/signup.js',              //entry
       output: {
-        path: path.resolve(__dirname, 'Static/dist'),    //output where to put/what to name webpack folder
+        path: path.resolve(__dirname, 'static/dist'),    //output where to put/what to name webpack folder
         filename: 'signup.js'
       },
     },
